ed94658dafd66ba6997da507b984ff9900ade88a
TomaEduard <eduard.daniel.toma@gmail.com>
2019-08-24 14:50:20 +0300

    added jdbc properties, datasource, reading file and authentication

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,17 +1,35 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
 
+	// set up variable to hold the properties
+	@Autowired
+	private Environment env;
+
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
 	// define a bean for ViewResolver
 
 	@Bean
@@ -25,6 +43,49 @@ public class DemoAppConfig {
 		return viewResolver;
 	}
 	
+	// define a bean for our security datasource
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// create connection pool
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		
+		// set jdbc driver class
+		try {
+			comboPooledDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		
+		// log the connection props - optional
+		logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+		logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+		
+		// set database connection props
+		comboPooledDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		comboPooledDataSource.setUser(env.getProperty("jdbc.user"));
+		comboPooledDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		// set connection pool props
+		comboPooledDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		comboPooledDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		comboPooledDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		comboPooledDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		return comboPooledDataSource;
+		 
+		// need a helper method
+		//read enviroment property and convert to int
+		
+	}
+	
+	private int getIntProperty(String propName) {
+		
+		String propVal = env.getProperty(propName);
+		
+		int intPropVal = Integer.parseInt(propVal);
+		
+		return intPropVal;
+	}
 }
 
 
